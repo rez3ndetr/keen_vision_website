41f06258c1b4121f0f7f4d0035495bfb55a8a47d
rezendetr <sanyaboykov61@gmail.com>
2024-03-27 13:06:39 +0300

    awesome commit

--- keenvision/settings.py
+++ keenvision/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-from config import KEY
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY
+SECRET_KEY = 'django-insecure-3@d(3_ei7eq3d9#6cu*#edxgy$a4a!l044^hm$zhu3(yr_twig'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
